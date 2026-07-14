7e5d4582da596a84240d9350214bf4a366de8d69
argosenpaikun <affianonn@hotmail.com>
2026-07-14 19:04:37 +0800

    Update modules

--- src/api/documents.py
+++ src/api/documents.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from models.documents import AddDocumentRequest
-from milvus.document import insert_document
+from models.documents import DeleteDocumentRequest
+from milvus.document import insert_document, delete_document
 from services.bm25_service import rebuild_index
 
 router = APIRouter(
     prefix="/documents",
     tags=["Documents"],
 )
-
 
 @router.post("/")
 def add_document(request: AddDocumentRequest):
@@ -24,3 +24,23 @@ def add_document(request: AddDocumentRequest):
         "insert_count": result["insert_count"],
         "ids": result["ids"],
     }
+
+@router.delete("/")
+def remove_document(request: DeleteDocumentRequest):
+    try:
+        delete_document(
+            collection_name=request.collection_name,
+            document_id=request.document_id
+        )
+
+        rebuild_index()
+
+        return {
+            "status": "success",
+            "messsage": f"Document {request.document_id} deleted successfully"
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
--- src/models/documents.py
+++ src/models/documents.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class AddDocumentRequest(BaseModel):
     collection_name: str
     text: str
+
+class DeleteDocumentRequest(BaseModel):
+    collection_name: str
+    document_id: int
