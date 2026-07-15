3fa0e7c430fe60365f88750311ecd6603b92f755
argosenpaikun <affianonn@hotmail.com>
2026-07-16 06:52:27 +0800

    Update modules

--- src/main.py
+++ src/main.py
@@ -62,11 +62,8 @@ def main():
         "main:create_app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
         factory=True
     )
-
-app = create_app()
 
 if __name__ == "__main__":
     main()
--- src/telemetry/opentelemetry.py
+++ src/telemetry/opentelemetry.py
@@ -2,7 +2,7 @@ from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 resource = Resource.create(
@@ -17,8 +17,7 @@ provider = TracerProvider(resource=resource)
 trace.set_tracer_provider(provider)
 
 otl_exporter = OTLPSpanExporter(
-    endpoint="http://otel-collector:4317",
-    insecure=True
+    endpoint="http://localhost:4318/v1/traces"
 )
 
 provider.add_span_processor(
